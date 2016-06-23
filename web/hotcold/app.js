378101eee0bc599d734e4dd697a10967e4f84e3b
Macon Pegram <mpegram3rd@gmail.com>
2016-06-22 22:22:17 -0400

    Modified hot/cold algorithm.

--- web/hotcold/app.js
+++ web/hotcold/app.js
@@ -1,16 +1,17 @@
 var BIGGEST_NUMBER = 100;
+var divisionSize = 15.0 / BIGGEST_NUMBER;
 var myNumber = -1;
-var lastGuess = null;
-var temperatureText = ['so close', 'a total bonfire', 'on fire', 'burning up', 'really hot',
+var temperatureText = ['like a volcano', 'a total bonfire', 'on fire', 'burning up', 'really hot',
                  'starting to sweat', 'feeling toasty', 'seeing some sparks',
                  'feeling a slight breeze', 'getting chilly', 'shivering', 'in need of a coat',
                  'freezing', 'icy', 'a block of ice'];
 
 function startGame() {
+    showMessage('');
     document.getElementById('start-game').style.display='none';
     document.getElementById('guessing-form').style.display='block';
-    lastGuess = null;
     myNumber = parseInt(1 + Math.random() * BIGGEST_NUMBER);
+    divisionSize = calculateDivisionSize(myNumber);
     console.log('My number is: ' + myNumber + ' shhhhhh!  Don\'t tell!');
 }
 
@@ -29,6 +30,11 @@ function playGame(formIn) {
         return false;
     }
 
+    if (guess < 1 || guess > BIGGEST_NUMBER) {
+        showMessage('Please choose a number between 1 and ' + BIGGEST_NUMBER);
+        return false;
+    }
+
     if (guess == myNumber) {
         showMessage('You won!');
         endGame();
@@ -36,9 +42,12 @@ function playGame(formIn) {
     }
 
     var severity =  calculateSeverity(myNumber, guess);
-    showMessage('You are ' + temperatureText[severity])
+    var distance = calculateDistance(myNumber, guess);
+    var message = distance < 3 ? 'very close!' : temperatureText[severity];
+    showMessage('You are ' + message);
     changeBackground('guess-' + severity);
     formIn.guess.focus();
+    formIn.guess.select();
 
     return false;
 }
@@ -47,23 +56,28 @@ function calculateDistance(number1, number2) {
     return Math.abs(number1 - number2);
 }
 
+function calculateDivisionSize(secretNumber) {
+
+    // See if secret number is closer to the biggest number then
+    // the greatest distance is the distance from 0 to the secret number.
+    if (secretNumber > BIGGEST_NUMBER / 2) {
+        divisionSize = 15.0 / secretNumber;
+    }
+    else {
+        // Otherwise the the division size will be based on the distance from
+        // the secret number to the biggest number.
+        divisionSize = 15.0 / (BIGGEST_NUMBER - secretNumber);
+    }
+
+    return divisionSize;
+
+}
 // Figures out how severely HOT or COLD your guess is.
 function calculateSeverity(secretNumber, guess) {
 
     // First let's figure out the distance between the two numbers
     var distance = calculateDistance(secretNumber, guess);
-    
-    var divisionSize = 1;
-    
-    // Now let's divide the range above our secret number and below it into 5 equal segments to assign severity.
-    // If our guess is ABOVE the secret number, then we divide up 5 / (MAX_NUMBER - secretNumber)
-    if (guess > secretNumber) {
-        divisionSize = 15.0 / (BIGGEST_NUMBER - secretNumber);
-    }
-    else {  // This handles the case where I guess was too low
-        divisionSize = 15.0 / secretNumber;
-    }
-    
+
     // now we take the distance and multiple it by our division size to get a gauge of how far away their answer is.
     return parseInt(divisionSize * distance);
 }
